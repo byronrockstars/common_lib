71d7119972629e6df523368e956b0f70e7a7d624
byronrockstars <byronrockstars@gmail.com>
2026-06-04 17:41:44 -0500

    combined library
    1. Combined library
    2. Test script

--- edu/byron/bioglow/Sanvi_Test_Project.py
+++ edu/byron/bioglow/Sanvi_Test_Project.py
@@ -1,24 +1,16 @@
 from hub import light_matrix, motion_sensor, port, sound
 import runloop, motor, motor_pair, sys, time
+import Combined as rockstar_lib
 
 LARGE_MOTOR_MAX_VELOCITY = 1050
 
-class MyDefaults:
-    def __init__(self, port1, port2):
-        self.port1 = port1
-        self.port2 = port2
-
-def moveForward(degreesToMove):
-    print("In moveForward function, degrees to move = " +str(degreesToMove))
-    
-    motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesToMove, 0, velocity=263, stop=motor.BRAKE, acceleration=500, deceleration=1000)
-
-    return
-
 async def main():
-    myDef = MyDefaults(port.A, port.E)
+    myConfig = RobotConfig(name = "Pez",  
+        mainPortLeft = port.A,
+        mainPortRight = port.E,        
+    )
 
-    motor_pair.pair(motor_pair.PAIR_1, myDef.port1, myDef.port2)
+    motor_pair.pair(motor_pair.PAIR_1, myConfig.mainPortLeft, myConfig.mainPortRight)
 
     moveForward(360)
     return

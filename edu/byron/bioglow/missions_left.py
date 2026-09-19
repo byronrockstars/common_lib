1e856aa4924c2c22f8275200ba7238f0fa928e11
byronrockstars/By..G..b1 <byronrockstars/By...Be..>
2026-09-19 15:01:42 -0500

    Create single file for left missions

---
+++ edu/byron/bioglow/missions_left.py
@@ -0,0 +1,24 @@
+from hub import port
+import runloop
+import color
+import Combined as RW
+
+
+async def runMission3() -> None:
+    #Mission 3 (Flip the Rock)
+    await RW.moveForward(rotations=2.38, velocityPercentage=40)
+    await RW.moveBackward(rotations=2.35, velocityPercentage=100, acceleration=10000, deceleration=4000) #acceleration/deceleration of 10,000 is max
+    return
+
+
+async def main():
+
+    robot = RW.initializeRobot(
+        name="Misty",
+        mainPortLeft=port.A,
+        mainPortRight=port.B,
+    )
+
+    await runMission3()
+    
+runloop.run(main())

38ab1e39072c3ef69220195bbc8f405c5772c1d5
byronrockstars/By..G..b1 <byronrockstars/By...Be..>
2026-09-19 15:08:14 -0500

    Updated multi-line docstrings to not break exporter.

--- edu/byron/bioglow/Combined.py
+++ edu/byron/bioglow/Combined.py
@@ -15,12 +15,12 @@ def __turnCompleted(degreesToTurn) -> bool:
     return abs(motion_sensor.tilt_angles()[0] * -0.1) >= abs(degreesToTurn)
 
 async def _pivotTurn(degreesToTurn, velocity) -> None:
-    """Completes a pivot turn up to 179 degrees. 
-
-            Input parameters:
-                degreesToTurn: positive value if turning to right and negative if turning to left
-                velocity: (deg/sec) Medium motor range = -1110 to 1110
-    """
+    #Completes a pivot turn up to 179 degrees. 
+    #
+    #        Input parameters:
+    #            degreesToTurn: positive value if turning to right and negative if turning to left
+    #            velocity: (deg/sec) Medium motor range = -1110 to 1110
+    #
     print('In pivotTurn, degreesToTurn = ' + str(degreesToTurn) + ', velocity = ' + str(velocity) + '.')
     motion_sensor.reset_yaw(0)
     time.sleep(0.1)
@@ -34,12 +34,12 @@ async def _pivotTurn(degreesToTurn, velocity) -> None:
     return
 
 async def _spinTurn(degreesToTurn, velocity) -> None:
-    """Completes a spin turn up to 179 degrees. 
-
-            Input parameters:
-                degreesToTurn: positive value if turning to right and negative if turning to left
-                velocity: (deg/sec) Medium motor range = -1110 to 1110
-    """
+    #Completes a spin turn up to 179 degrees. 
+    #
+    #        Input parameters:
+    #            degreesToTurn: positive value if turning to right and negative if turning to left
+    #            velocity: (deg/sec) Medium motor range = -1110 to 1110
+    #
     print('In spinTurn, degreesToTurn = ' + str(degreesToTurn) + ', velocity = ' + str(velocity) + '.')
     motion_sensor.reset_yaw(0)
     time.sleep(0.1)
@@ -53,13 +53,13 @@ async def _spinTurn(degreesToTurn, velocity) -> None:
     return
 
 async def _arcTurn(radiusInCm, degreesToTurn, velocityPercentage=20) -> None:
-    """Completes an arc turn up to 179 degrees. 
- 
-            Input parameters:   
-                radiusInCm: radius of circle (in centimeters) that robot moves while making its arc turn. A value of 0 equates to a spin turn (use dedicated spin turn functions instead).
-                degreesToTurn: positive value if turning to right and negative if turning to left
-                velocityPercentage (optional): how fast to complete the turn in percentage (1 to 100). Negative values move backward.
-    """
+    #Completes an arc turn up to 179 degrees. 
+    #
+    #        Input parameters:   
+    #            radiusInCm: radius of circle (in centimeters) that robot moves while making its arc turn. A value of 0 equates to a spin turn (use dedicated spin turn functions instead).
+    #            degreesToTurn: positive value if turning to right and negative if turning to left
+    #            velocityPercentage (optional): how fast to complete the turn in percentage (1 to 100). Negative values move backward.
+    #
     print('Arc Turn. radiusInCm = ' + str(radiusInCm) + ', degreesToTurn = ' + str(degreesToTurn) + ', velocityPercentage = ' + str(velocityPercentage) + '.')
     velocity = velocityPercentage / 100 * WHEEL_MOTOR_MAX_VELOCITY
     halfWheelBase = WHEEL_BASE_IN_CENTIMETERS / 2.0
@@ -78,13 +78,13 @@ async def _arcTurn(radiusInCm, degreesToTurn, velocityPercentage=20) -> None:
     return
 
 async def _proportionalPivotTurn(degreesToTurn, velocityPercentage=40, timeout=2.0) -> None:
-    """Complete a pivot turn and slow down as the turn completes to ensure accuracy. 
-
-            Input parameters: 
-                degreesToTurn: positive value if turning to right and negative if turning to left (-179 to 179)
-                velocityPercentage (optional): how fast to complete the turn in percentage (1 to 100)
-                timeout (optional): maximum number of seconds to allow turn to complete. If turn doesn't complete in this time, stop the turn when timeout is reached.
-    """
+    #Complete a pivot turn and slow down as the turn completes to ensure accuracy. 
+    #
+    #        Input parameters: 
+    #            degreesToTurn: positive value if turning to right and negative if turning to left (-179 to 179)
+    #            velocityPercentage (optional): how fast to complete the turn in percentage (1 to 100)
+    #            timeout (optional): maximum number of seconds to allow turn to complete. If turn doesn't complete in this time, stop the turn when timeout is reached.
+    #
     print('Proportional Pivot Turn. DegreesToTurn = ' + str(degreesToTurn) + '. velocityPercentage = ' + str(velocityPercentage) + ', timeout(seconds) = ' + str(timeout))
     motion_sensor.reset_yaw(0)
     time.sleep(0.1)
@@ -109,13 +109,13 @@ async def _proportionalPivotTurn(degreesToTurn, velocityPercentage=40, timeout=2
     return
 
 async def _proportionalSpinTurn(degreesToTurn, velocityPercentage=30, timeout=2.0) -> None:
-    """Complete a spin turn and slow down as the turn completes to ensure accuracy. 
-
-        Input parameters:
-            degreesToTurn: positive value if turning to right and negative if turning to left (-179 to 179)
-            velocityPercentage (optional): how fast to complete the turn in percentage (1 to 100)
-            timeout (optional): maximum number of seconds to allow turn to complete. If turn doesn't complete in this time, stop the turn when timeout is reached.
-    """
+    #Complete a spin turn and slow down as the turn completes to ensure accuracy. 
+    #
+    #    Input parameters:
+    #        degreesToTurn: positive value if turning to right and negative if turning to left (-179 to 179)
+    #        velocityPercentage (optional): how fast to complete the turn in percentage (1 to 100)
+    #        timeout (optional): maximum number of seconds to allow turn to complete. If turn doesn't complete in this time, stop the turn when timeout is reached.
+    #
     print('Proportional Spin Turn. DegreesToTurn = ' + str(degreesToTurn) + '. velocityPercentage = ' + str(velocityPercentage) + ', timeout(seconds) = ' + str(timeout))
     motion_sensor.reset_yaw(0)
     time.sleep(0.1)
@@ -140,14 +140,14 @@ async def _proportionalSpinTurn(degreesToTurn, velocityPercentage=30, timeout=2.
     return
 
 async def _moveForward(stoppingRotations, velocityPercentage, acceleration=500, deceleration=1000) -> None:
-    """Moves straight (without using the gyro sensor). 
-
-            Input parameters:
-                stoppingRotations: positive value if going forward and negative if going backward
-                velocityPercentage: 0% to 100%.
-                acceleration (optional): (deg/sec^2) Default is 500.
-                deceleration (optional): (deg/sec^2) Default is 1000.
-    """
+    #Moves straight (without using the gyro sensor). 
+    #
+    #        Input parameters:
+    #            stoppingRotations: positive value if going forward and negative if going backward
+    #            velocityPercentage: 0% to 100%.
+    #            acceleration (optional): (deg/sec^2) Default is 500.
+    #            deceleration (optional): (deg/sec^2) Default is 1000.
+    #
     print('In moveForward function, rotations to move = ' + str(stoppingRotations) + ', velocityPercentage = ' + str(velocityPercentage) + ', acceleration = ' + str(acceleration) + ', deceleration = ' + str(deceleration) + '.')
     degreesToMove = stoppingRotations * 360
     velocity = WHEEL_MOTOR_MAX_VELOCITY * velocityPercentage / 100
@@ -193,16 +193,16 @@ async def __moveBackwardProporational(rotations, velocity, acceleration=500, bra
     return
 
 async def _moveStraightWheelRotation(stoppingRotations, velocityPercentage, acceleration=500, brakeStartValue=0.9, correctionMultiplier=-3.5) -> None:
-    """Moves straight using the gyro sensor to correct drift. 
-
-        Input parameters:
-            stoppingRotations: positive value if going forward and negative if going backward
-            velocityPercentage: 0 to +100.
-            acceleration (optional): (deg/sec^2) Default is 500.
-            brakeStartValue (optional): Decimal percentage of the driven distance after which the robot starts braking.
-            correctionMultiplier (optional): Used to determine how sharply to correct drift. Must be negative, and lower values make sharper corrections.
-                                             Typical values are -1 to -5.
-    """
+    #Moves straight using the gyro sensor to correct drift. 
+    #
+    #    Input parameters:
+    #        stoppingRotations: positive value if going forward and negative if going backward
+    #        velocityPercentage: 0 to +100.
+    #        acceleration (optional): (deg/sec^2) Default is 500.
+    #        brakeStartValue (optional): Decimal percentage of the driven distance after which the robot starts braking.
+    #        correctionMultiplier (optional): Used to determine how sharply to correct drift. Must be negative, and lower values make sharper corrections.
+    #                                         Typical values are -1 to -5.
+    #
     print('MoveStraightWheelRotations. Stopping Rotations =' + str(stoppingRotations) + '. Velocity % = ' + str(velocityPercentage) + ', Acceleration = ' + str(acceleration) + ', Brake Start Value = ' + str(brakeStartValue) + ', Correction Multiplier = ' + str(correctionMultiplier) + '.')
     velocity = WHEEL_MOTOR_MAX_VELOCITY * abs(velocityPercentage) / 100
     if stoppingRotations > 0:
@@ -224,17 +224,17 @@ def __whiteLineFound(leftLightSensorPort, rightLightSensorPort, bothSensorsOnLin
         return color_sensor.reflection(leftLightSensorPort) > WHITE_LINE_LIGHT_REFLECTION or color_sensor.reflection(rightLightSensorPort) > WHITE_LINE_LIGHT_REFLECTION
 
 async def _moveStraightUntilLine(leftLightSensorPort, rightLightSensorPort, lineColor, bothSensorsOnLine=False, velocityPercentage=25, acceleration=500) -> int:
-    """Moves straight ahead until one of the two light sensors finds the line with the inputted line color. 
-
-        Input parameters:
-            leftLightSensorPort: port number of left light sensor (ex port.B)
-            rightLightSensorPort: port number of right light sensor (ex. port.D)
-            lineColor: color of line to stop at (color.BLACK or color.WHITE)
-            bothSensorsOnLine (optional): True if robot should run until both light sensors are on line. False if robot should stop once first light sensor is on line.
-            velocityPercentage (optional): how fast (-100% to 100%) to move in a straight line. Negative values move backwards.
-            acceleration (optional): (deg/sec^2) Default is 500.
-            Note: ideal height of light sensor off of ground is 16mm (2 Lego blocks)
-    """
+    #Moves straight ahead until one of the two light sensors finds the line with the inputted line color. 
+    #
+    #    Input parameters:
+    #        leftLightSensorPort: port number of left light sensor (ex port.B)
+    #        rightLightSensorPort: port number of right light sensor (ex. port.D)
+    #        lineColor: color of line to stop at (color.BLACK or color.WHITE)
+    #        bothSensorsOnLine (optional): True if robot should run until both light sensors are on line. False if robot should stop once first light sensor is on line.
+    #        velocityPercentage (optional): how fast (-100% to 100%) to move in a straight line. Negative values move backwards.
+    #        acceleration (optional): (deg/sec^2) Default is 500.
+    #        Note: ideal height of light sensor off of ground is 16mm (2 Lego blocks)
+    #
     print('In moveStraightUntilLine function, left light sensor port = ' + str(leftLightSensorPort) + ', right light sensor port = ' + str(rightLightSensorPort) + ', line color = ' + str(lineColor) + ', velocityPercentage = ' + str(velocityPercentage) + ', acceleration = ' + str(acceleration) + '.')
     velocity = WHEEL_MOTOR_MAX_VELOCITY * velocityPercentage / 100
     motor_pair.move(motor_pair.PAIR_1, 0, velocity=int(velocity), acceleration=acceleration)
@@ -260,15 +260,15 @@ async def _moveStraightUntilLine(leftLightSensorPort, rightLightSensorPort, line
     return triggeredSensorPort
 
 async def _getSecondLightSensorOnLine(leftLightSensorPort, rightLightSensorPort, lineColor, velocityPercentage=25, acceleration=500) -> None:
-    """@deprecated("Use squareUpOnLine instead.")
-        Rotates robot until second light sensor finds the colored line.
-            Input parameters:
-                leftLightSensorPort: port where left light sensor is connected (ex. port.B)
-                rightLightSensorPort: port where right light sensor is connected (ex. port.D)
-                lightColor: color of line to search for (color.BLACK or color.WHITE)
-                velocityPercentage (optional): 0% to 100%.
-                acceleration (optional): (deg/sec^2) Default is 500.
-    """
+    #@deprecated("Use squareUpOnLine instead.")
+    #    Rotates robot until second light sensor finds the colored line.
+    #        Input parameters:
+    #            leftLightSensorPort: port where left light sensor is connected (ex. port.B)
+    #            rightLightSensorPort: port where right light sensor is connected (ex. port.D)
+    #            lightColor: color of line to search for (color.BLACK or color.WHITE)
+    #            velocityPercentage (optional): 0% to 100%.
+    #            acceleration (optional): (deg/sec^2) Default is 500.
+    #
     print('In getSecondLightSensorOnLine function, left light sensor port = ' + str(leftLightSensorPort) + ', right light sensor port = ' + str(rightLightSensorPort) + ', line color = ' + str(lineColor) + ', velocityPercentage = ' + str(velocityPercentage) + ', acceleration = ' + str(acceleration) + '.')
     velocity = WHEEL_MOTOR_MAX_VELOCITY * velocityPercentage / 100
     if lineColor == color.BLACK:
@@ -297,15 +297,15 @@ async def _getSecondLightSensorOnLine(leftLightSensorPort, rightLightSensorPort,
     return
 
 async def _squareUpOnLine(leftLightSensorPort, rightLightSensorPort, lineColor, velocityPercentage=15, acceleration=500) -> None:
-    """Squares up robot on black line by stopping each motor as its corresponding light sensor sees the black line. For best results, 
-       run at lower speeds and back robot off line after first call and run a second time.
-        Input parameters:
-            leftLightSensorPort: port where left light sensor is connected (ex. port.B)
-            rightLightSensorPort: port where right light sensor is connected (ex. port.D)
-            lineColor: color of line to stop at (color.BLACK or color.WHITE)
-            velocityPercentage (optional): 0% to 100%.
-            acceleration (optional): (deg/sec^2) Default is 500.
-    """
+    #Squares up robot on black line by stopping each motor as its corresponding light sensor sees the black line. For best results, 
+    #   run at lower speeds and back robot off line after first call and run a second time.
+    #    Input parameters:
+    #        leftLightSensorPort: port where left light sensor is connected (ex. port.B)
+    #        rightLightSensorPort: port where right light sensor is connected (ex. port.D)
+    #        lineColor: color of line to stop at (color.BLACK or color.WHITE)
+    #        velocityPercentage (optional): 0% to 100%.
+    #        acceleration (optional): (deg/sec^2) Default is 500.
+    #
     print('squareUpOnLine leftLightSensorPort = ' + str(leftLightSensorPort) + ', rightLightSensorPort = ' + str(rightLightSensorPort) + ', lineColor = ' + str(lineColor) + ', velocityPercentage = ' + str(velocityPercentage) + ', acceleration = ' + str(acceleration) + '.')
     velocity = WHEEL_MOTOR_MAX_VELOCITY * velocityPercentage / 100
     motor.run(LEFT_WHEEL_PORT, int(-velocity), acceleration=acceleration)
@@ -332,19 +332,19 @@ async def _squareUpOnLine(leftLightSensorPort, rightLightSensorPort, lineColor, 
     return
 
 async def _pidBlackLineFollow(rotationsToMove, lightSensorPort, midPointReflectionPercentage, edgeToFollow, proportionalCorrectionCoef=0.15, integralCorrectionCoef=0.0, derivativeCorrectionCoef=0.0, velocityPercentage=10, acceleration=500) -> None:
-    """Follows black line using the light sensor. Meant to be used for forward movement only currently. 
-
-        Input parameters:
-            rotationsToMove: Number of rotations to move while following line.
-            lightSensorPort: port where light sensor to use for following black line is connected (ex. port.B)
-            midPointReflectionPercentage: Average of white and black reflection readings (in percentage) from the light sensor on the board.
-            edgeToFollow: whether to follow the black line on the left (BLACK_LINE_LEFT_EDGE) or right (BLACK_LINE_RIGHT_EDGE)
-            proportionalCorrectionCoef: value between 0 and 1 that determines how aggressively to make corrections to movement (Tune this first.)
-            integralCorrectionCoef: corrects steady drift (Tune this last. Should be small value ex. 0.05)
-            derivativeCorrectionCoef: smoothes overcorrections (Tune this second after proportionalCorrectionCoef. Ex. 3.0)
-            velocityPercentage (optional): 0% to 100%.
-            acceleration (optional): (deg/sec^2) Default is 500.
-    """
+    #Follows black line using the light sensor. Meant to be used for forward movement only currently. 
+    #
+    #    Input parameters:
+    #        rotationsToMove: Number of rotations to move while following line.
+    #        lightSensorPort: port where light sensor to use for following black line is connected (ex. port.B)
+    #        midPointReflectionPercentage: Average of white and black reflection readings (in percentage) from the light sensor on the board.
+    #        edgeToFollow: whether to follow the black line on the left (BLACK_LINE_LEFT_EDGE) or right (BLACK_LINE_RIGHT_EDGE)
+    #        proportionalCorrectionCoef: value between 0 and 1 that determines how aggressively to make corrections to movement (Tune this first.)
+    #        integralCorrectionCoef: corrects steady drift (Tune this last. Should be small value ex. 0.05)
+    #        derivativeCorrectionCoef: smoothes overcorrections (Tune this second after proportionalCorrectionCoef. Ex. 3.0)
+    #        velocityPercentage (optional): 0% to 100%.
+    #        acceleration (optional): (deg/sec^2) Default is 500.
+    #
     print('In pidBlackLineFollow, rotationsToMove = ' + str(rotationsToMove) + ', lightSensorPort = ' + str(lightSensorPort) + ', midPointReflectionPercentage = ' + str(midPointReflectionPercentage) + ', edgeToFollow = ' + str(edgeToFollow) + ', proportionalCorrectionCoef = ' + str(proportionalCorrectionCoef) + ', integralCorrectionCoef = ' + str(integralCorrectionCoef) + ', derivativeCorrectionCoef = ' + str(derivativeCorrectionCoef) + ', velocity% = ' + str(velocityPercentage) + ', acceleration = ' + str(acceleration) + '.')
     velocity = WHEEL_MOTOR_MAX_VELOCITY * velocityPercentage / 100
     degreesToMove = rotationsToMove * 360

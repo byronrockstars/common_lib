fae5f665c09984e7e9bf1059457b22686e65b8d3
byronrockstars <byronrockstars@gmail.com>
2026-09-23 17:28:48 -0700

    added comments
    We added a comment stating the success rate of "flip the rock"

--- edu/byron/bioglow/missions_left.py
+++ edu/byron/bioglow/missions_left.py
@@ -6,9 +6,10 @@ import Combined as RW
 
 async def runMission3() -> None:
     #Mission 3 (Flip the Rock)
+    # Works 100% of the time even with interesting obstacles sometimes
     await RW.moveForward(rotations=2.38, velocityPercentage=40)
     await RW.moveBackward(rotations=2.35, velocityPercentage=100, acceleration=10000, deceleration=4000) #acceleration/deceleration of 10,000 is max
-    return
+    return  
 
 
 async def main():
@@ -20,5 +21,5 @@ async def main():
     )
 
     await runMission3()
-    
+
 runloop.run(main())
